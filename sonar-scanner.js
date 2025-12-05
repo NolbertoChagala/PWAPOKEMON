eaac609db6c65a7eacd16aa802ffa2315726283d
NolbertoChagala <21393137@utcancun.edu.mx>
2025-12-04 20:26:28 -0500

    chore: agregar sonarqube-scanner y script de análisis

--- sonar-scanner.js
+++ sonar-scanner.js
@@ -5,12 +5,14 @@ scanner(
     serverUrl: 'http://sonarqube:9000',
     token: process.env.SONAR_TOKEN,
     options: {
-      'sonar.projectKey': 'pokedex-pwa',       // igual al Project Key de SonarQube
-      'sonar.projectName': 'Pokedex PWA',      // Nombre del proyecto
-      'sonar.sources': 'src',                   // Carpeta con tu código React
+      'sonar.projectKey': 'pokedex-pwa',
+      'sonar.projectName': 'Pokedex PWA',
+      'sonar.sources': 'src',
       'sonar.language': 'js',
       'sonar.sourceEncoding': 'UTF-8'
     }
   },
-  () => {}
+  () => {
+    process.exit();
+  }
 );
